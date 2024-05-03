5eb9f63d3bd9da82eb851741fd481ba1c1015c26
CitizensofPlanetEarth <141430945+CitizensofPlanetEarth@users.noreply.github.com>
2024-05-03 12:45:08 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 (function() {
-
-
 }).call(this);
 
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiPGFub255bW91cz4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0FBQUEiLCJzb3VyY2VzQ29udGVudCI6WyIiXX0=
@@ -11,7 +9,6 @@ var animateButton = function(e) {
   e.preventDefault;
   //reset animation
   e.target.classList.remove('animate');
-  
   e.target.classList.add('animate');
   setTimeout(function(){
     e.target.classList.remove('animate');
@@ -19,7 +16,6 @@ var animateButton = function(e) {
 };
 
 var bubblyButtons = document.getElementsByClassName("bubbly-button");
-
 for (var i = 0; i < bubblyButtons.length; i++) {
   bubblyButtons[i].addEventListener('click', animateButton, false);
 }
